80c3ee813fb68de3d6b8830d4fcd5df563c5dd48
EDMPhoenix <greatm1257@gmail.com>
2024-01-08 18:13:02 -0500

    fix: Update game.js

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+// IMPORTANT: Make sure to use Kaboom version 0.5.0 for this game by adding the correct script tag in the HTML file.
+
 kaboom({
   global: true,
   fullscreen: true,
@@ -6,12 +8,15 @@ kaboom({
   clearColor: [0,0,0,1],
 });
 
+// Speed Identifiers
 const MOVE_SPEED = 120;
 const JUMP_FORCE = 360;
 const BIG_JUMP_FORCE = 550;
 let CURRENT_JUMP_FORCE = JUMP_FORCE;
 const FALL_DEATH = 400;
 const ENEMY_SPEED = 20;
+
+// Game Logic
 
 let isJumping = true;
 
